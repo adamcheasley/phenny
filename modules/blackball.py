fc29a10b94e43ad432d6a0f2b8687f4e6440d38e
Adam Cheasley <adam.cheasley@gmail.com>
2011-06-17 16:42:05 +0100

    key the user who blackballed by host rather than nick. This way a user can only black ball someone else once.

--- modules/blackball.py
+++ modules/blackball.py
@@ -3,7 +3,7 @@
 blackball_data = []
 
 
-def blackball(phenny, input):
+def blackball(phenny, input, origin):
     """
     allow other users to 'blackball' users.
     If a user receives 3 blackballs, they are banned
@@ -12,7 +12,7 @@ def blackball(phenny, input):
     if input.sender.startswith('#'): 
         return phenny.say('pm .blackball <username> to nominate someone anonymously for a blackball-style ban from the channel.')
     blackballee = input.group(2)
-    blackballer = input.sender
+    blackballer = origin.host
     if not blackballee:
         return phenny.say('Who would you like to blackball?')
     blackballed_users = [x['nick'] for x in blackball_data]
@@ -20,12 +20,15 @@ def blackball(phenny, input):
         blackball_data.append({'nick' : blackballee,
                                'blackballed_by': [blackballer]
                                })
+        ball_count = 1
     else:
         for user in blackball_data:
             if user['nick'] == blackballee:
+                if blackballer in user['blackballed_by']:
+                    return phenny.say('You may only blackball a user once.')
                 user['blackballed_by'].append(blackballer)
                 ball_count = len(user['blackballed_by'])
-    phenny.reply('Thank you. The user %s has been blackballed' % blackballee)
+    phenny.say('Thank you. The user %s has been blackballed' % blackballee)
     print 'A user was blackballed'
     print 'ball count for user %s is %i' % (blackballee,
                                             ball_count)
