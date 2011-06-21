af856116a2f54027c90816e668d4780621fe0f31
Adam Cheasley <adam.cheasley@gmail.com>
2011-06-21 14:15:47 +0100

    We need one method to record all chat and one for the command.

--- modules/chat.py
+++ modules/chat.py
@@ -3,13 +3,29 @@
 chat_data = []
 
 
+def get_chat(phenny, input):
+    """
+    records the chat
+    """
+    raw_chat = input
+    # do not record the command itself
+    if raw_chat == u'.chat':
+        return
+    if len(chat_data) > 50:
+        chat_data.pop(0)
+    chat_data.append(raw_chat)
+
+get_chat.rule = r'(.*)'
+get_chat.priority = 'low'
+
+
 def chat(phenny, input):
     """
     tells the user who has been chatting and about what
     """
-    phenny.say('working')
+    phenny.say('Well')
+    phenny.say('People have been talking about %s' % chat_data)
 
 chat.commands = ['chat']
-chat.priority = 'high'
-
+chat.priority = 'low'
 
