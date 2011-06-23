dff553cdf7a32d9ecb09773e89273ab3d5c0207b
Adam Cheasley <adam.cheasley@gmail.com>
2011-06-23 12:09:26 +0100

    remove commented code

--- modules/seen.py
+++ modules/seen.py
@@ -10,6 +10,7 @@ http://inamidst.com/phenny/
 import time
 from tools import deprecated
 
+
 @deprecated
 def f_seen(self, origin, match, args): 
    """.seen <nick> - Reports when <nick> was last seen."""
@@ -29,6 +30,7 @@ def f_seen(self, origin, match, args):
    else: self.msg(origin.sender, "Sorry, I haven't seen %s around." % nick)
 f_seen.rule = (['seen'], r'(\S+)')
 
+
 @deprecated
 def f_note(self, origin, match, args): 
    def note(self, origin, match, args): 
@@ -38,15 +40,11 @@ def f_note(self, origin, match, args):
          # if origin.sender == '#inamidst': return
          self.seen[origin.nick.lower()] = (origin.sender, time.time())
 
-      # if not hasattr(self, 'chanspeak'): 
-      #    self.chanspeak = {}
-      # if (len(args) > 2) and args[2].startswith('#'): 
-      #    self.chanspeak[args[2]] = args[0]
-
    try: note(self, origin, match, args)
    except Exception, e: print e
 f_note.rule = r'(.*)'
 f_note.priority = 'low'
+
 
 if __name__ == '__main__': 
    print __doc__.strip()
