6e8ce4db0a0b591ea6ac182ccb307a2cc04af470
Adam Cheasley <adam@netsight.co.uk>
2012-09-04 15:30:32 +0100

    remove unused function

--- modules/ping.py
+++ modules/ping.py
@@ -8,7 +8,7 @@ About: http://inamidst.com/phenny/
 import random
 
 
-def hello(phenny, input): 
+def hello(phenny, input):
    greeting = random.choice(('Hi', 'Hey', 'Hello',
                              'yo', 'word up', 'what up', 
                              'sup', 'yeah'))
@@ -17,29 +17,11 @@ def hello(phenny, input):
 hello.rule = r'(?i)(hi|hello|hey|yo|sup|ohai) $nickname\b'
 
 
-def interjection(phenny, input): 
+def interjection(phenny, input):
    phenny.say(input.nick + '!')
 interjection.rule = r'$nickname!'
 interjection.priority = 'high'
 interjection.thread = False
-
-
-def greeting(phenny, input):
-   # XXX by pass this for now.
-   # DO NOT CHECK IN
-   return
-   # XXX take into account the users blackball count and greet accordingly
-   if input.nick == phenny.nick:
-      return
-   # only greet people sometimes
-   rand_num = random.randint(0, 3)
-   if rand_num <= 2:
-      return
-   greeting = random.choice(('Hi', 'Welcome', 'Good day', 'Nice to see you',
-                             'Hello', 'Yo'))
-   phenny.say('%s %s' % (greeting, input.nick))
-greeting.event = 'JOIN'
-greeting.rule = r'.*'
 
 
 if __name__ == '__main__': 
